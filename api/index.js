d56f9bb1d012fe0f5fcd14f47a26d05b7ed6d074
alekseignatovich-sketch <alekseignatovich@gmail.com>
2025-12-18 16:13:19 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 // api/index.js
 export default async function handler(req, res) {
-  if (req.method !== ' 'POST') {
+  if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 

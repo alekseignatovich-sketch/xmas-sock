b553bace185e2de5d5a8eb6b948d1be1bbf718e3
alekseignatovich-sketch <alekseignatovich@gmail.com>
2025-12-23 17:00:17 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ function readBody(req) {
   });
 }
 
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
